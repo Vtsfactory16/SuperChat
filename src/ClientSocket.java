4af892859b8deb50d0b1591d1e1f0974843d4e94
Red One <saxovtsfactory16@gmail.com>
2024-01-25 17:40:24 +0100

    Add Image

--- src/ClientSocket.java
+++ src/ClientSocket.java
@@ -28,59 +28,46 @@ public class ClientSocket {
     }
 
     public void start () throws UnknownHostException, IOException {
-        //System.out.println("(Cliente) Estableciendo conexión...");
         socket = new Socket(serverIP,serverPort);
         os = socket.getOutputStream();
         is = socket.getInputStream();
-        //System.out.println("(Cliente) Conexión establecida.");
     }
 
     public void stop () throws IOException {
-        //System.out.println("(Cliente) Cerrando conexiones.");
         is.close();
         os.close();
         socket.close();
-        //System.out.println(" (Cliente) Conexiones cerradas.");
     }
 
     // abrimos los canales de lectura y de escritura - Igual que en el servidor
     public void abrirCanalesDeTexto() {
-        //System.out.println(" (Cliente) Abriendo canales de texto...");
         //Canales de lectura
         isr = new InputStreamReader(is);
         br = new BufferedReader(isr);
         //Canales de escritura
         pw = new PrintWriter(os, true);
-        //System.out.println("(Cliente) Cerrando canales de texto.");
     }
 
     // cerramos los canales de lectura y de escritura - Igual que en el servidor
     public void cerrarCanalesDeTexto() throws IOException {
-        //System.out.println(" (Cliente) Cerrando canales de texto...");
         //Canales de lectura
         br.close();
         isr.close();
         //Canales de escritura
         pw.close();
-        //System.out.println("(Cliente) Cerrando canales de texto.");
     }
 
     public String leerMensajeTexto()  throws IOException {
-        //System.out.println(" (Cliente) Leyendo mensaje...");
         String mensaje = br.readLine();
-        //System.out.println(" (Cliente) Mensaje leido.");
         return mensaje;
     }
 
     public void enviarMensajeTexto(String mensaje) {
-        //System.out.println(" (Cliente) Enviando mensaje...");
         pw.println(mensaje);
-        //System.out.println(" (Cliente) Mensaje enviado.");
     }
 
 
     public static void main (String[] args) {
-
 
         String mensaje;
 
