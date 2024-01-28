a90e2cf4be145091dfd77178a4ddef0e13ceb80c
Red One <saxovtsfactory16@gmail.com>
2024-01-28 18:18:55 +0100

    Implementacion Intefaz

---
+++ src/HiloCliente.java
@@ -0,0 +1,181 @@
+
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.util.LinkedList;
+
+/**
+ * Clase encargada de manejar la comunicación con un cliente en particular.
+ * Cada cliente que se conecta al servidor tiene un hilo que se encarga de
+ * escuchar lo que el cliente dice y de enviarle mensajes.
+ */
+public class HiloCliente extends Thread{
+    /**
+     * Socket que se utiliza para comunicarse con el cliente.
+     */
+    private final Socket socket;
+    /**
+     * Stream con el que se envían objetos al servidor.
+     */
+    private ObjectOutputStream objectOutputStream;
+    /**
+     * Stream con el que se reciben objetos del servidor.
+     */
+    private ObjectInputStream objectInputStream;
+    /**
+     * Servidor al que pertenece este hilo.
+     */
+    private final Servidor server;
+    /**
+     * Identificador único del cliente con el que este hilo se comunica.
+     */
+    private String identificador;
+    /**
+     * Variable booleana que almacena verdadero cuando este hilo esta escuchando
+     * lo que el cliente que atiende esta diciendo.
+     */
+    private boolean escuchando;
+    /**
+     * Método constructor de la clase hilo cliente.
+     * @param socket
+     * @param server
+     */
+    public HiloCliente(Socket socket,Servidor server) {
+        this.server=server;
+        this.socket = socket;
+        try {
+            objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+            objectInputStream = new ObjectInputStream(socket.getInputStream());
+        } catch (IOException ex) {
+            System.err.println("Error en la inicialización del ObjectOutputStream y el ObjectInputStream");
+        }
+    }
+    /**
+     * Método que cierra el socket de comunicación con el cliente.
+     */
+    public void desconnectar() {
+        try {
+            socket.close();
+            escuchando=false;
+        } catch (IOException ex) {
+            System.err.println("Error al cerrar el socket de comunicación con el cliente.");
+        }
+    }
+    /**
+     * Método que se ejecuta cuando se inicia el hilo.
+     */
+    public void run() {
+        try{
+            escuchar();
+        } catch (Exception ex) {
+            System.err.println("Error al llamar al método readLine del hilo del cliente.");
+        }
+        desconnectar();
+    }
+
+    /**
+     * Método que se encarga de escuchar lo que el cliente dice y de enviarle mensajes.
+     */
+    public void escuchar(){
+        escuchando=true;
+        while(escuchando){
+            try {
+                Object aux=objectInputStream.readObject();
+                if(aux instanceof LinkedList){
+                    ejecutar((LinkedList<String>)aux);
+                }
+            } catch (Exception e) {
+                System.err.println("Error al leer lo enviado por el cliente.");
+            }
+        }
+    }
+    /**
+     * Método que se encarga de ejecutar las acciones que el cliente solicita.
+     * @param lista
+     */
+    public void ejecutar(LinkedList<String> lista){
+
+        String tipo=lista.get(0);
+        switch (tipo) {
+            case "SOLICITUD_CONEXION":
+
+                confirmarConexion(lista.get(1));
+                break;
+            case "SOLICITUD_DESCONEXION":
+
+                confirmarDesConexion();
+                break;
+            case "MENSAJE":
+
+                String destinatario=lista.get(2);
+                server.clientes
+                        .stream()
+                        .filter(h -> (destinatario.equals(h.getIdentificador())))
+                        .forEach((h) -> h.enviarMensaje(lista));
+                break;
+            default:
+                break;
+        }
+    }
+    /**
+     * Método para enviar un mensaje al cliente através del socket.
+     * @param lista
+     */
+    private void enviarMensaje(LinkedList<String> lista){
+        try {
+            objectOutputStream.writeObject(lista);
+        } catch (Exception e) {
+            System.err.println("Error al enviar el objeto al cliente.");
+        }
+    }
+    /**
+     * Método que se encarga de confirmar la conexión del cliente.
+     * @param identificador
+     */
+    private void confirmarConexion(String identificador) {
+        Servidor.correlativo++;
+        this.identificador=Servidor.correlativo+" - "+identificador;
+        LinkedList<String> lista=new LinkedList<>();
+        lista.add("CONEXION_ACEPTADA");
+        lista.add(this.identificador);
+        lista.addAll(server.getUsuariosConectados());
+        enviarMensaje(lista);
+        server.agregarLog("\nNuevo cliente: "+this.identificador);
+        //enviar a todos los clientes el nombre del nuevo usuario conectado excepto a él mismo
+        LinkedList<String> auxLista=new LinkedList<>();
+        auxLista.add("NUEVO_USUARIO_CONECTADO");
+        auxLista.add(this.identificador);
+        server.clientes
+                .stream()
+                .forEach(cliente -> cliente.enviarMensaje(auxLista));
+        server.clientes.add(this);
+    }
+    /**
+     * Método que devuelve el identificador del cliente.
+     * @return
+     */
+    public String getIdentificador() {
+        return identificador;
+    }
+    /**
+     * Método que se encarga de confirmar la desconexión del cliente.
+     */
+    private void confirmarDesConexion() {
+        LinkedList<String> auxLista=new LinkedList<>();
+        auxLista.add("USUARIO_DESCONECTADO");
+        auxLista.add(this.identificador);
+        server.agregarLog("\nEl cliente \""+this.identificador+"\" se ha desconectado.");
+        this.desconnectar();
+        for(int i=0;i<server.clientes.size();i++){
+            if(server.clientes.get(i).equals(this)){
+                server.clientes.remove(i);
+                break;
+            }
+        }
+        server.clientes
+                .stream()
+                .forEach(h -> h.enviarMensaje(auxLista));
+    }
+}
---
+++ src/Servidor.java
@@ -0,0 +1,85 @@
+
+
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.LinkedList;
+import javax.swing.JOptionPane;
+
+/**
+ * Clase en la que se maneja la comunicación del lado del servidor, esta clase
+ * es la que se encarga de escuchar las conexiones de los clientes y de crear
+ * un hilo para cada cliente que se conecta, dicho hilo es el que se encarga de
+ * escuchar lo que el cliente dice y de enviar mensajes a dicho cliente.
+ */
+public class Servidor extends Thread{
+    /**
+     * Socket utilizado para escuchar las conexiones de los clientes.
+     */
+    private ServerSocket serverSocket;
+    /**
+     * Lista de hilos de clientes que se conectan al servidor.
+     */
+    LinkedList<HiloCliente> clientes;
+    /**
+     * Variable que almacena la ventana que gestiona la interfaz gráfica del servidor.
+     */
+    private final VentanaS ventana;
+    /**
+     * Variable que almacena el puerto que el servidor usará para escuchar.
+     */
+    private final String puerto;
+    /**
+     * Variable que almacena el identificador del cliente que se conecta.
+     */
+    static int correlativo;
+    /**
+     * Constructor del servidor.
+     * @param puerto
+     * @param ventana
+     */
+    public Servidor(String puerto, VentanaS ventana) {
+        correlativo=0;
+        this.puerto=puerto;
+        this.ventana=ventana;
+        clientes=new LinkedList<>();
+        this.start();
+    }
+    /**
+     * Método que devuelve el puerto que el servidor usará para escuchar.
+     */
+    public void run() {
+        try {
+            serverSocket = new ServerSocket(Integer.valueOf(puerto));
+            ventana.addServidorIniciado();
+            while (true) {
+                HiloCliente h;
+                Socket socket;
+                socket = serverSocket.accept();
+                System.out.println("Nueva conexion entrante: "+socket);
+                h=new HiloCliente(socket, this);
+                h.start();
+            }
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(ventana, "El servidor no se ha podido iniciar,\n"
+                    + "puede que haya ingresado un puerto incorrecto.\n"
+                    + "Esta aplicación se cerrará.");
+            System.exit(0);
+        }
+    }
+    /**
+     * Método que envía un mensaje a todos los clientes conectados al servidor.
+     * @return
+     */
+    LinkedList<String> getUsuariosConectados() {
+        LinkedList<String>usuariosConectados=new LinkedList<>();
+        clientes.stream().forEach(c -> usuariosConectados.add(c.getIdentificador()));
+        return usuariosConectados;
+    }
+    /**
+     * Método que agrega una linea al log de la interfaz gráfica del servidor.
+     * @param texto
+     */
+    void agregarLog(String texto) {
+        ventana.agregarLog(texto);
+    }
+}
